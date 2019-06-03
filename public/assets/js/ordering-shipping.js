8e17e5ddb7be49c7c2ffbd54b09d130905b9920e
albi_frattini <alberto.frattini@mail.polimi.it>
2019-06-03 15:01:28 +0200

    Documentation base

--- public/assets/js/ordering-shipping.js
+++ public/assets/js/ordering-shipping.js
@@ -7,14 +7,3 @@ document.getElementById("shippingCosts").innerHTML = "Your order will ship with 
 document.getElementById("internationalShipping").innerHTML = "Your order will ship with Standard Shipping unless you select an alternative. If you don't receive a shipping confirmation email right away, don't worry! We will deliver the items to you by the delivery date shown or within the date range provided at checkout. We do not ship Saturdays, Sundays, or holidays. If your order requires additional information to fully process, we will be in touch by email.";
 
 document.getElementById("whatToKnow").innerHTML = "Your order will ship with Standard Shipping unless you select an alternative. If you don't receive a shipping confirmation email right away, don't worry! We will deliver the items to you by the delivery date shown or within the date range provided at checkout. We do not ship Saturdays, Sundays, or holidays. If your order requires additional information to fully process, we will be in touch by email.";
-
-function addStaticLinks() {
-
-	console.log('Adding static links.');
-
-	const homeUrl = window.location.origin;
-	$('#home').attr('href', homeUrl);
-	$('#events').attr('href', homeUrl+'/pages/events.html');
-}
-
-addStaticLinks();
