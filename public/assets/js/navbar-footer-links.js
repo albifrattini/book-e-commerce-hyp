91a631e7a3686eee69eacc939516413f3161434c
albi_frattini <alberto.frattini@mail.polimi.it>
2019-06-13 10:53:50 +0200

    Registration js

--- public/assets/js/navbar-footer-links.js
+++ public/assets/js/navbar-footer-links.js
@@ -19,37 +19,6 @@ function addStaticLinks() {
 } 
 
 
-function sendData() {
-
-    var http = new XMLHttpRequest();
-
-    var params =  "email=" + document.getElementById('inputEmail').value +
-                  "&password=" + document.getElementById('inputPassword').value;
-
-
-    // Raises and Alert with some informations and goes back to index.html!
-    http.addEventListener("load", function(event) {
-        let resp = JSON.parse(event.target.responseText);
-        alert(`${resp.info}, ${resp.user.name}!`);
-        window.location = window.location.origin;
-    });
-
-    // Define what happens in case of error
-    http.addEventListener("error", function(event) {
-        alert(event.target.responseText);
-    });
-
-    // Set up our request
-    http.open("POST", "/v2/users/login", true);
-
-    http.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-
-    // The data sent is what the user provided in the form
-    http.send(params);
-
-}
-
-
 function closeToggle() {
 	console.log('ciao');
 	var element = document.getElementById('navbar-toggle');
